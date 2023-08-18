d8225bf2838f8a2fe1df4acbe542f6dc700646a2
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-18 20:43:28 +0300

    Unit of work structure added.
    Visit https://github.com/Arch/UnitOfWork

---
+++ Libraries/SM.Infrastructre/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query;
+using SM.Core.Interfaces.Repositores;
+using SM.Infrastructre.Persistence.Contexts;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SM.Infrastructre.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SMDbContext _context;
+        private bool disposed = false;
+
+        public UnitOfWork(SMDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            return new Repository<TEntity>(_context);
+        }
+
+        /// <summary>
+        /// Executes the specified raw SQL command.
+        /// </summary>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The number of state entities written to database.</returns>
+        public int ExecuteSqlCommand(string sql, params object[] parameters) => _context.Database.ExecuteSqlRaw(sql, parameters);
+
+        /// <summary>
+        /// Uses raw SQL queries to fetch the specified <typeparamref name="TEntity" /> data.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>An <see cref="IQueryable{T}" /> that contains elements that satisfy the condition specified by raw SQL.</returns>
+        public IQueryable<TEntity> FromSql<TEntity>(string sql, params object[] parameters) where TEntity : class => _context.Set<TEntity>().FromSqlRaw(sql, parameters);
+
+        /// <summary>
+        /// Saves all changes made in this context to the database.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
+        /// <returns>The number of state entries written to the database.</returns>
+        public int SaveChanges(bool ensureAutoHistory = false)
+        {
+            if (ensureAutoHistory)
+            {
+                _context.EnsureAutoHistory();
+            }
+
+            return _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Asynchronously saves all changes made in this unit of work to the database.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
+        /// <returns>A <see cref="Task{TResult}"/> that represents the asynchronous save operation. The task result contains the number of state entities written to database.</returns>
+        public async Task<int> SaveChangesAsync(bool ensureAutoHistory = false)
+        {
+            if (ensureAutoHistory)
+            {
+                _context.EnsureAutoHistory();
+            }
+
+            return await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">The disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // dispose the db context.
+                    _context.Dispose();
+                }
+            }
+
+            disposed = true;
+        }
+
+        public void TrackGraph(object rootEntity, Action<EntityEntryGraphNode> callback)
+        {
+            _context.ChangeTracker.TrackGraph(rootEntity, callback);
+        }
+    }
+}
