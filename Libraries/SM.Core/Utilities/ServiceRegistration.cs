c0024722626cc524c2237f07d2282ab562214fbb
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-18 23:39:12 +0300

    FluentValidation and Validation behavior added.

--- Libraries/SM.Core/Utilities/ServiceRegistration.cs
+++ Libraries/SM.Core/Utilities/ServiceRegistration.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using SM.Core.Common.Behaviors;
 using SM.Core.Mappers.AutoMapper;
+using SM.Core.Validators.Auth;
 
 namespace SM.Core.Utilities
 {
@@ -16,6 +20,15 @@ namespace SM.Core.Utilities
             #region Automapper
 
             services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            #endregion
+
+            #region Validation
+
+            services.AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>())
+                .Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+
+            services.AddControllers(options => options.Filters.Add<ValidationBehavior>());
 
             #endregion
         }
