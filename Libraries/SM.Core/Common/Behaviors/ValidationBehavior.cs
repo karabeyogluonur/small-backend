098a898754e73d1081159c4c95c46d439cb08030
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-18 23:39:12 +0300

    FluentValidation and Validation behavior added.

---
+++ Libraries/SM.Core/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SM.Core.Common.Behaviors
+{
+    public class ValidationBehavior : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.ModelState.IsValid)
+                await next();
+
+            var errors = context.ModelState.Where(modelState => modelState.Value.Errors.Any())
+                .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => e.ErrorMessage))
+                .ToArray();
+            
+            context.Result = new BadRequestObjectResult(ApiResponse<object>.Error(errors, "The sent object is incorrect."));
+
+            return;
+        }
+
+    }
+}
---
+++ Libraries/SM.Core/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SM.Core.Features.Auth.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Core.Validators.Auth
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(login => login.Email).EmailAddress().NotNull().NotEmpty();
+            RuleFor(login => login.Password).NotNull().NotEmpty();
+
+        }
+    }
+}
