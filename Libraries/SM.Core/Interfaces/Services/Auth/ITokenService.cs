2c891f8cd61dac8e6b5938fbdd7b374c754ef7fb
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-18 21:16:53 +0300

    Token service created.

---
+++ Libraries/SM.Core/Interfaces/Services/Auth/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using SM.Core.Domain;
+using SM.Core.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Core.Interfaces.Services.Auth
+{
+    public interface ITokenService
+    {
+        TokenDTO CreateToken(ApplicationUser applicationUser);
+    }
+}
