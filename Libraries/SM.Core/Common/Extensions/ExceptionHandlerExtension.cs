e26ffbb79c481b60b8a03fb7f08b39aa149c34cb
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-22 22:16:12 +0300

    Exception handler added.

---
+++ Libraries/SM.Core/Common/Extensions/ExceptionHandlerExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace SM.Core.Common.Extensions
+{
+    public static class ExceptionHandlerExtension
+    {
+        public static void ConfigureExceptionHandler<T>(this WebApplication application)
+        {
+            application.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(new ApiResponse<object>
+                        {
+                            Success = false,
+                            Message = contextFeature.Error.Message,
+                            Data = null,
+                            Errors = null
+                        }));
+                    }
+                });
+            });
+        }
+    }
+}
