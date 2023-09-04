33820f0b68b63f6eafaa94f09d1b1d0ed38dfaa2
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-04 13:18:57 +0300

    Add author check for incoming publish request.

--- Libraries/SM.Core/Features/Articles/PublishArticle/PublishArticleHandler.cs
+++ Libraries/SM.Core/Features/Articles/PublishArticle/PublishArticleHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using SM.Core.Common;
 using SM.Core.Domain;
+using SM.Core.Interfaces.Services.Auth;
 using SM.Core.Interfaces.Services.Blog;
 
 namespace SM.Core.Features.Articles.PublishArticle
@@ -9,10 +10,12 @@ namespace SM.Core.Features.Articles.PublishArticle
     public class PublishArticleHandler : IRequestHandler<PublishArticleRequest, ApiResponse<PublishArticleResponse>>
     {
         private readonly IArticleService _articleService;
+        private readonly IAuthService _authService;
 
-        public PublishArticleHandler(IArticleService articleService)
+        public PublishArticleHandler(IArticleService articleService, IAuthService authService)
         {
             _articleService = articleService;
+            _authService = authService;
         }
 
         public async Task<ApiResponse<PublishArticleResponse>> Handle(PublishArticleRequest request, CancellationToken cancellationToken)
@@ -22,7 +25,12 @@ namespace SM.Core.Features.Articles.PublishArticle
             if (article == null || article.Deleted)
                 return ApiResponse<PublishArticleResponse>.Error(null, "Article is not found");
 
-            if(article.Published)
+            ApplicationUser author = await _authService.GetAuthenticatedCustomerAsync();
+
+            if (article.AuthorId == author.Id)
+                throw new UnauthorizedAccessException("You are not authorized for this article.");
+
+            if (article.Published)
                 return ApiResponse<PublishArticleResponse>.Error(null, "The article has already been published.");
 
             if (string.IsNullOrEmpty(article.Title) || string.IsNullOrEmpty(article.Content))
