152258a16d8fcbd2f45484dd5c0127f5a66f447d
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-08 13:23:58 +0300

    Profile 'Me' endpoint added.

---
+++ Libraries/SM.Core/Features/Profiles/Me/MeRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+using SM.Core.Common;
+
+namespace SM.Core.Features.Profiles.Me
+{
+	public class MeRequest : IRequest<ApiResponse<MeResponse>>
+	{
+		
+	}
+}
+
---
+++ Libraries/SM.Core/Features/Profiles/Me/MeResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SM.Core.DTOs.Membership;
+
+namespace SM.Core.Features.Profiles.Me
+{
+	public class MeResponse
+	{
+		public ApplicationUserDTO User { get; set; }
+	}
+}
+
