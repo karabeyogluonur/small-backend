476c599a9a09c5bec9ac9b8b8e6e504163dbda27
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-21 00:16:11 +0300

    Move swagger from development to production.

--- Presentation/SM.API/Program.cs
+++ Presentation/SM.API/Program.cs
@@ -3,6 +3,8 @@ using SM.Core.Utilities;
 using SM.Infrastructre.Utilities;
 using SM.Infrastructre.Persistence.Seeds;
 using SM.Core.Common.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    OpenApiSecurityScheme jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+        Reference = new OpenApiReference
+        {
+           Id = JwtBearerDefaults.AuthenticationScheme,
+           Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+
+});
 
 #endregion
 
@@ -37,12 +63,18 @@ builder.Services.Configure<EmailAccountOptions>(builder.Configuration.GetSection
 
 var app = builder.Build();
 
+#region Swagger
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+#endregion
+
 #region Development
 
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 #endregion
