be624b6ab0b9e880909e0aed735260961eb7eae9
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-21 19:17:51 +0300

    Article 'CheckArticleLike' endpoint added.

---
+++ Libraries/SM.Core/Features/Articles/CheckArticleLike/CheckArticleLikeHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MediatR;
+using SM.Core.Common;
+using SM.Core.Domain;
+using SM.Core.Interfaces.Services;
+using SM.Core.Interfaces.Services.Blog;
+
+namespace SM.Core.Features.Articles.CheckArticleLike
+{
+    public class CheckArticleLikeHandler : IRequestHandler<CheckArticleLikeRequest, ApiResponse<CheckArticleLikeResponse>>
+    {
+        private readonly IArticleService _articleService;
+        private readonly IWorkContext _workContext;
+
+        public CheckArticleLikeHandler(IArticleService articleService, IWorkContext workContext)
+        {
+            _articleService = articleService;
+            _workContext = workContext;
+        }
+
+        public async Task<ApiResponse<CheckArticleLikeResponse>> Handle(CheckArticleLikeRequest request, CancellationToken cancellationToken)
+        {
+            int authorId = await _workContext.GetAuthenticatedUserIdAsync();
+
+            ArticleLike articleLike = await _articleService.GetArticleLikeAsync(authorId: authorId, articleId: request.ArticleId);
+
+            if (articleLike == null)
+                return ApiResponse<CheckArticleLikeResponse>.Successful(new CheckArticleLikeResponse() { IsLike = false }, "Article liking process has been checked successfully.");
+            else
+                return ApiResponse<CheckArticleLikeResponse>.Successful(new CheckArticleLikeResponse() { IsLike = true }, "Article liking process has been checked successfully.");
+        }
+    }
+}
+
---
+++ Libraries/SM.Core/Features/Articles/CheckArticleLike/CheckArticleLikeResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SM.Core.Features.Articles.CheckArticleLike
+{
+	public class CheckArticleLikeResponse
+	{
+		public bool IsLike { get; set; }
+	}
+}
+
