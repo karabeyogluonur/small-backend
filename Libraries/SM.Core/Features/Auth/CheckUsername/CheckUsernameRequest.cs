9d4f6c0833b7c53c25a5c8ba9d01ddd766651829
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-25 14:52:18 +0300

    Auth 'CheckUsername' endpoint added.

---
+++ Libraries/SM.Core/Features/Auth/CheckUsername/CheckUsernameRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+using SM.Core.Common;
+
+namespace SM.Core.Features.Auth.CheckUsername
+{
+	public class CheckUsernameRequest : IRequest<ApiResponse<CheckUsernameResponse>>
+	{
+		public string Username { get; set; }
+	}
+}
+
---
+++ Libraries/SM.Core/Features/Auth/CheckUsername/CheckUsernameResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SM.Core.Features.Auth.CheckUsername
+{
+	public class CheckUsernameResponse
+	{
+		public bool isAvailable { get; set; }
+	}
+}
+
